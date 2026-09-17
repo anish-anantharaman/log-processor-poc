2c92b7093a9e7c1828df0cbc3cfde18d1414e901
anish-anantharaman <anish.pgt@gmail.com>
2026-09-17 13:16:00 +0530

    feat: implement solace config and raw log ingestion

--- src/main/kotlin/com/anish/logprocessor/ingestion/subscriber/RawLogSubscriber.kt
+++ src/main/kotlin/com/anish/logprocessor/ingestion/subscriber/RawLogSubscriber.kt
@@ -1,19 +1,55 @@
 package com.anish.logprocessor.ingestion.subscriber
 
+import com.anish.logprocessor.ingestion.dmq.DmqPublisher
+import com.anish.logprocessor.ingestion.mapper.LogEventMapper
+import com.anish.logprocessor.ingestion.parser.RawLogParser
+import com.anish.logprocessor.model.DmqMessage
+import com.anish.logprocessor.solace.publisher.ProcessedLogPublisher
 import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
+import java.time.Instant
 import java.util.function.Consumer
 
 @Component
-class RawLogSubscriber {
+class RawLogSubscriber(
+    private val rawLogParser: RawLogParser,
+    private val logEventMapper: LogEventMapper,
+    private val processedLogPublisher: ProcessedLogPublisher,
+    private val dmqPublisher: DmqPublisher
+    ) {
     companion object {
         private val log = LoggerFactory.getLogger(RawLogSubscriber::class.java)
     }
 
-    fun processRawLog(): Consumer<String> {
-        return Consumer{ rawLog ->
-            log.info("Received raw log: {}", rawLog)
+    @Bean
+    fun rawLogSubscriber(): Consumer<String> {
+        return Consumer{ payload ->
+            log.info("Received raw log: {}", payload)
+                process(payload)
+        }
+    }
 
+    private fun process(payload: String) {
+        try {
+            // 1. Parse and validate
+            val rawLog = rawLogParser.parseAndValidate(payload)
+
+            // 2. Build canonical LogEvent
+            val logEvent = logEventMapper.toLogEvent(rawLog, payload);
+
+            // 3. Publish successful message
+            processedLogPublisher.publish(logEvent);
+        } catch (e: Exception) {
+
+            // 4. Publish invalid message to DMQ
+            val dmqMessage = DmqMessage(
+                payload,
+                e.message ?: "Unknown processing error",
+                "PARSE_AND_VALIDATE",
+                Instant.now().toString()
+            )
+            dmqPublisher.publish(dmqMessage)
         }
     }
 }
---
+++ src/main/kotlin/com/anish/logprocessor/solace/config/SolaceConfig.kt
@@ -0,0 +1,6 @@
+package com.anish.logprocessor.solace.config
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SolaceConfig
